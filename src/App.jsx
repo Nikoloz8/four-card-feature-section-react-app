f5a7e04af4ba33cef00e826e5ffaacef05f1fa53
Nikoloz Tevdoradze <nikoloztevdoradze08@gmail.com>
2025-05-09 22:29:07 +0400

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -21,21 +21,21 @@ function App() {
         </div>
         <div className="column">
           <Box
-            borderColor={"#44D3D2"}
-            h3Text={"Supervisor"}
-            text={'Monitors activity to identify project roadblocks'}
+            borderColor={"#EA5454"}
+            h3Text={"Team Builder"}
+            text={'Scans our talent network to create the optimal team for your project'}
             image={IconTeamBuilder} />
           <Box
-            borderColor={"#44D3D2"}
-            h3Text={"Supervisor"}
-            text={'Monitors activity to identify project roadblocks'}
+            borderColor={"#FCAE4A"}
+            h3Text={"Karma"}
+            text={'Regularly evaluates our talent to ensure quality'}
             image={IconKarma} />
         </div>
         <div className="column">
           <Box
-            borderColor={"#44D3D2"}
-            h3Text={"Supervisor"}
-            text={'Monitors activity to identify project roadblocks'}
+            borderColor={"#549EF2"}
+            h3Text={"Calculator"}
+            text={'Uses data from past projects to provide better delivery estimates'}
             image={IconCalculator} />
         </div>
       </div>
