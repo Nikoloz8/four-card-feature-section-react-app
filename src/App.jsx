a490666fd4bb35194e3c24907b376a26371d09c4
Nikoloz Tevdoradze <nikoloztevdoradze08@gmail.com>
2025-05-09 22:26:12 +0400

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css';
 import Box from "./components/box/Box.jsx"
 import TextContainer from "./components/text/Text.jsx"
-import IconCalculator from "../../four-card-feature-section - react/src/assets/icon-calculator.svg"
-import IconKarma from "../../four-card-feature-section - react/src/assets/icon-karma.svg"
-import IconSupervisor from "../../four-card-feature-section - react/src/assets/icon-supervisor.svg"
-import IconTeamBuilder from "../../four-card-feature-section - react/src/assets/icon-team-builder.svg"
+import IconCalculator from "./assets/icon-calculator.svg"
+import IconKarma from "./assets/icon-karma.svg"
+import IconSupervisor from ".assets/icon-supervisor.svg"
+import IconTeamBuilder from "./assets/icon-team-builder.svg"
 
 
 function App() {
